cdce757651318f9fd77e0f1935cf47e2b34f00fe
Miocasa <narimanabdualiev06@gmail.com>
2025-08-26 16:16:23 +0500

    added some comments

--- lily58_my/config.h
+++ lily58_my/config.h
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
+// Old type of definition renamed
 // #define ENCODERS_PAD_A  GP29
 // #define ENCODERS_PAD_B  GP28
 // #define ENCODERS_PAD_A_RIGHT  GP28
